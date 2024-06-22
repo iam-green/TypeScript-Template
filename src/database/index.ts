e1f056c03926c72566e3a071dcab153d52b8f790
Pastel <yundohyun050121@gmail.com>
2024-06-22 16:13:25 +0900

    add: `database`

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Log } from '../module';
+import { Client } from 'pg';
+import * as schema from '../database/schema';
+import 'dotenv/config';
+
+const queryClient = new Client(process.env.DATABASE_URL || '');
+
+export const databaseInit = async () => {
+  if (process.env.DATABASE_URL) {
+    await queryClient.connect();
+    Log.info('Database Connected');
+  }
+};
+
+export const db = drizzle(queryClient, { schema });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
+import { databaseInit } from './database';
 
 async function bootstrap() {
+  await databaseInit();
   // Input Code Here
 }
 bootstrap();
