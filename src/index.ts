399137f4203cc405f56dc6f7f7a6c1e674e8a7be
Pastel <yundohyun050121@gmail.com>
2024-06-22 15:43:26 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import { Log } from './module';
+
+dotenv.config();
+
+async function bootstrap() {
+  // Input Code Here
+}
+bootstrap();
---
+++ src/module/log.ts
@@ -0,0 +1,39 @@
+import 'colors';
+
+export class Log {
+  static _date() {
+    const date = new Date();
+    const year = date.getFullYear().toString().padStart(4, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
+  }
+
+  static info(content: string) {
+    console.log(
+      `${'['.cyan}${'INFO'.green}${']'.cyan} ${'('.cyan}${this._date().yellow}${')'.cyan} ${content}`,
+    );
+  }
+
+  static debug(content: string) {
+    if (process.env.NODE_ENV == 'development')
+      console.log(
+        `${'['.cyan}${'DEBUG'.magenta}${']'.cyan} ${'('.cyan}${this._date().yellow}${')'.cyan} ${content}`,
+      );
+  }
+
+  static warn(content: string) {
+    console.log(
+      `${'['.cyan}${'WARN'.yellow}${']'.cyan} ${'('.cyan}${this._date().yellow}${')'.cyan} ${content}`,
+    );
+  }
+
+  static error(content: string) {
+    console.log(
+      `${'['.cyan}${'ERROR'.red}${']'.cyan} ${'('.cyan}${this._date().yellow}${')'.cyan} ${content.red}`,
+    );
+  }
+}
