1d106c0ee8097d0706ed36c11937e9c9a769d471
iam-green <yundohyun050121@gmail.com>
2024-08-25 16:09:35 +0900

    fix: `database/index.ts`
    database migration 오류 수정

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,16 +2,14 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Log } from '../module';
 import { Client } from 'pg';
-import * as schema from '../database/schema';
+import * as schema from './schema';
 
 const queryClient = new Client(process.env.DATABASE_URL || '');
 
 export const databaseInit = async () => {
   if (process.env.DATABASE_URL) {
     await queryClient.connect();
-    await migrate(db, {
-      migrationsFolder: `${__dirname.replace(/\\/g, '/')}/migration`,
-    });
+    await migrate(db, { migrationsFolder: `./src/database/migration` });
     Log.info('Database Connected');
   }
 };
