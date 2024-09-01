0a28bb608cafed3735794d4c328a1e3f6b2d52be
iam-green <yundohyun050121@gmail.com>
2024-09-01 14:44:22 +0900

    edit: `example` schema
    `created` column에 timezone 포함

--- src/database/schema/example.ts
+++ src/database/schema/example.ts
@@ -2,5 +2,5 @@ import { pgTable, uuid, timestamp } from 'drizzle-orm/pg-core';
 
 export const example = pgTable('example', {
   id: uuid('id').primaryKey().defaultRandom(),
-  created: timestamp('created').notNull().defaultNow(),
+  created: timestamp('created', { withTimezone: true }).notNull().defaultNow(),
 });
