e0f40ddd6833d2109d42370e7ca94b492be947b7
iam-green <yundohyun050121@gmail.com>
2025-03-26 01:42:15 +0900

    chore: update dependencies

--- src/database/schema/example.ts
+++ src/database/schema/example.ts
@@ -1,6 +1,6 @@
 import { pgTable, uuid, timestamp } from 'drizzle-orm/pg-core';
 
 export const example = pgTable('example', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  created: timestamp('created', { withTimezone: true }).notNull().defaultNow(),
+  id: uuid().primaryKey().defaultRandom(),
+  created: timestamp({ withTimezone: true }).notNull().defaultNow(),
 });
